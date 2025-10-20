4b11e15f3c9ba07565f0e9f1ba75b91dcc3c27f8
hyunn522 <tjgus52213@gmail.com>
2025-10-20 12:36:15 +0900

    init spring project

---
+++ src/test/java/app/allstackproject/privideo/PrivideoApplicationTests.java
@@ -0,0 +1,13 @@
+package app.allstackproject.privideo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
