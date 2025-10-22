c74aa169ef4f9f76b982365350faaab3227e9a3c
hyunn522 <tjgus52213@gmail.com>
2025-10-22 17:17:09 +0900

    fix: 회원가입 요청에 전화번호 필드 추가

--- src/main/java/app/allstackproject/privideo/dto/user/PostSignupRequest.java
+++ src/main/java/app/allstackproject/privideo/dto/user/PostSignupRequest.java
@@ -19,7 +19,7 @@ public class PostSignupRequest {
     @NotBlank(message = "이름은 1글자 이상이어야 합니다.")
     private String name;
 
-    @NotBlank(message = "이메일을 입력해 주세요.")
+    @NotBlank(message = "이메일을 입력해주세요.")
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 
@@ -32,6 +32,9 @@ public class PostSignupRequest {
 
     @AgeTypeConstraint
     private Integer age;
+
+    @NotBlank(message = "비밀번호를 입력해주세요.")
+    private String phoneNumber;
 
     private String organizationCode;
 }
