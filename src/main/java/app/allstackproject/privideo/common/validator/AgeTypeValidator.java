a369fef58d7948bfe96075dea5977734721274e9
hyunn522 <tjgus52213@gmail.com>
2025-10-22 12:15:24 +0900

    hotfix: enum 값 기반으로 validate하도록 수정

--- src/main/java/app/allstackproject/privideo/common/validator/AgeTypeValidator.java
+++ src/main/java/app/allstackproject/privideo/common/validator/AgeTypeValidator.java
@@ -1,15 +1,25 @@
 package app.allstackproject.privideo.common.validator;
 
 import app.allstackproject.privideo.common.annotation.AgeTypeConstraint;
+import app.allstackproject.privideo.common.enumStatus.AgeType;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class AgeTypeValidator implements ConstraintValidator<AgeTypeConstraint, Integer> {
-    private static final Set<Integer> ALLOWED_AGES = Set.of(10, 20, 30, 40, 50);
+    private Set<Integer> allowed;
+
+    @Override
+    public void initialize(AgeTypeConstraint constraintAnnotation) {
+        allowed = Stream.of(AgeType.values())
+                .map(AgeType::getValue)
+                .collect(Collectors.toUnmodifiableSet());
+    }
 
     @Override
     public boolean isValid(Integer value, ConstraintValidatorContext context) {
-        return value != null && ALLOWED_AGES.contains(value);
+        return value != null && allowed.contains(value);
     }
 }
