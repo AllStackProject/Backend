61a5d53ef203478406649314608719bce529a459
hyunn522 <tjgus52213@gmail.com>
2025-10-23 10:25:01 +0900

    feat: PasswordEncoder 추가

--- src/main/java/app/allstackproject/privideo/common/config/SecurityConfig.java
+++ src/main/java/app/allstackproject/privideo/common/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,21 +14,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(c -> c.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(a -> a
-                        .requestMatchers("/error", "/favicon.ico").permitAll()
+                        .requestMatchers("/error", "/favicon.ico", "/h2-console", "/user/signup", "/user/login")
+                        .permitAll()
                         .anyRequest().permitAll() // 전부 열어둠
                 )
                 .formLogin(f -> f.disable())
-                .httpBasic(b -> b.disable())
-                .headers((headerConfig) ->
-                        headerConfig.frameOptions(frameOptionsConfig ->
-                                frameOptionsConfig.disable()
-                        )
-                );
+                .httpBasic(b -> b.disable());
 
         return http.build();
     }
