3ddf2a0fb0c27ac825a2a9738d2ff867ca392007
hyunn522 <tjgus52213@gmail.com>
2025-10-23 10:31:30 +0900

    fix: h2 console 사용 위해 Frame 설정 추가

--- src/main/java/app/allstackproject/privideo/common/config/SecurityConfig.java
+++ src/main/java/app/allstackproject/privideo/common/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,15 +23,20 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(c -> c.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers((headerConfig) ->
+                        headerConfig.frameOptions(FrameOptionsConfig::disable
+                        )
+                )
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(a -> a
-                        .requestMatchers("/error", "/favicon.ico", "/h2-console", "/user/signup", "/user/login")
+                        .requestMatchers("/error", "/favicon.ico", "/user/signup", "/user/login")
                         .permitAll()
-                        .anyRequest().permitAll() // 전부 열어둠
+                        .anyRequest().permitAll() // TODO: 임의로 모두 열어둠 -> 테스트 후 삭제
                 )
                 .formLogin(f -> f.disable())
                 .httpBasic(b -> b.disable());
+        ;
 
         return http.build();
     }
