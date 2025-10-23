12f3c90aec363bed57f23c6bdcd641d94f616be3
hyunn522 <tjgus52213@gmail.com>
2025-10-23 10:13:46 +0900

    feat: response json 직렬화 및 키 타입 변경

--- src/main/java/app/allstackproject/privideo/common/response/BaseErrorResponse.java
+++ src/main/java/app/allstackproject/privideo/common/response/BaseErrorResponse.java
@@ -1,14 +1,20 @@
 package app.allstackproject.privideo.common.response;
 
 import app.allstackproject.privideo.common.response.status.ResponseStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@JsonPropertyOrder({"code", "status", "message"})
 public class BaseErrorResponse implements ResponseStatus {
 
     private final int code;
+
+    @JsonIgnore
     private final HttpStatus httpStatus;
+
     private final String message;
 
     public BaseErrorResponse(ResponseStatus responseStatus) {
--- src/main/java/app/allstackproject/privideo/common/response/BaseResponse.java
+++ src/main/java/app/allstackproject/privideo/common/response/BaseResponse.java
@@ -3,16 +3,22 @@ package app.allstackproject.privideo.common.response;
 import static app.allstackproject.privideo.common.response.status.BaseExceptionResponseStatus.SUCCESS;
 
 import app.allstackproject.privideo.common.response.status.ResponseStatus;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
+@JsonPropertyOrder({"code", "status", "message", "result"})
 public class BaseResponse<T> implements ResponseStatus {
 
     private final int code;
+
+    @JsonIgnore
     private final HttpStatus httpStatus;
+
     private final String message;
 
     @JsonInclude(Include.NON_NULL)
