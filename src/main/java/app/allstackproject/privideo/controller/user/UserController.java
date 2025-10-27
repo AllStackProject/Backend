2b0c507f512656b24ff295b582a2ecaddf2a7833
hyunn522 <tjgus52213@gmail.com>
2025-10-27 10:19:48 +0900

    Feat: 로그인 기능 구현

--- src/main/java/app/allstackproject/privideo/controller/user/UserController.java
+++ src/main/java/app/allstackproject/privideo/controller/user/UserController.java
@@ -1,13 +1,18 @@
 package app.allstackproject.privideo.controller.user;
 
+import static app.allstackproject.privideo.common.filter.JwtAuthFilter.ACCESS_TOKEN_HEADER;
+import static app.allstackproject.privideo.common.filter.JwtAuthFilter.TOKEN_PREFIX;
+import static app.allstackproject.privideo.common.response.status.BaseExceptionResponseStatus.INVALID_USER_LOGIN;
 import static app.allstackproject.privideo.common.response.status.BaseExceptionResponseStatus.INVALID_USER_SIGNUP;
 import static app.allstackproject.privideo.common.util.BindingResultUtil.getErrorMessage;
 
 import app.allstackproject.privideo.common.exception.ApiException;
 import app.allstackproject.privideo.common.response.BaseResponse;
 import app.allstackproject.privideo.common.response.SuccessResponse;
+import app.allstackproject.privideo.dto.user.PostLoginRequest;
 import app.allstackproject.privideo.dto.user.PostSignupRequest;
 import app.allstackproject.privideo.service.user.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.BindingResult;
@@ -34,4 +39,19 @@ public class UserController {
         return new BaseResponse<>(SuccessResponse.of(isSuccess));
     }
 
+    @PostMapping("/login")
+    public BaseResponse<SuccessResponse> login(@Valid @RequestBody PostLoginRequest postLoginRequest,
+                                               BindingResult bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            throw new ApiException(INVALID_USER_LOGIN, getErrorMessage(bindingResult));
+        }
+
+        String accessToken = userService.login(postLoginRequest);
+        if (accessToken == null || accessToken.isBlank()) {
+            return new BaseResponse<>(SuccessResponse.of(false));
+        }
+
+        response.setHeader(ACCESS_TOKEN_HEADER, TOKEN_PREFIX + accessToken);
+        return new BaseResponse<>(SuccessResponse.of(true));
+    }
 }
