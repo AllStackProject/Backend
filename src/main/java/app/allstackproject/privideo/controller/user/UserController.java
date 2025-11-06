26527e85bcd3dcf878750a4c93baecbb924522a6
hyunn522 <tjgus52213@gmail.com>
2025-11-06 14:43:35 +0900

    feat: 중복 기능을 갖고 있는 API 삭제

--- src/main/java/app/allstackproject/privideo/controller/user/UserController.java
+++ src/main/java/app/allstackproject/privideo/controller/user/UserController.java
@@ -1,6 +1,5 @@
 package app.allstackproject.privideo.controller.user;
 
-import static app.allstackproject.privideo.common.config.SwaggerConfig.BOOTSTRAP_AUTH_KEY;
 import static app.allstackproject.privideo.common.filter.JwtAuthFilter.ACCESS_TOKEN_HEADER;
 import static app.allstackproject.privideo.common.filter.JwtAuthFilter.TOKEN_PREFIX;
 import static app.allstackproject.privideo.common.response.status.BaseExceptionResponseStatus.INVALID_USER_LOGIN;
@@ -10,21 +9,15 @@ import static app.allstackproject.privideo.common.util.BindingResultUtil.getErro
 import app.allstackproject.privideo.common.exception.ApiException;
 import app.allstackproject.privideo.common.response.BaseResponse;
 import app.allstackproject.privideo.common.response.SuccessResponse;
-import app.allstackproject.privideo.dto.user.PatchPasswordRequest;
 import app.allstackproject.privideo.dto.user.PostLoginRequest;
 import app.allstackproject.privideo.dto.user.PostSignupRequest;
 import app.allstackproject.privideo.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,15 +58,5 @@ public class UserController {
 
         response.setHeader(ACCESS_TOKEN_HEADER, TOKEN_PREFIX + accessToken);
         return new BaseResponse<>(SuccessResponse.of(true));
-    }
-
-    @Operation(summary = "유저 비밀번호 변경", security = {@SecurityRequirement(name = BOOTSTRAP_AUTH_KEY)})
-    @PreAuthorize("hasAuthority('bootstrap:granted')")
-    @PatchMapping("/password")
-    public BaseResponse<SuccessResponse> patchPassword(
-            @AuthenticationPrincipal(expression = "userId") Long userId,
-            @Valid @RequestBody PatchPasswordRequest patchPasswordRequest) {
-        boolean isSuccess = userService.patchPassword(userId, patchPasswordRequest);
-        return new BaseResponse<>(SuccessResponse.of(isSuccess));
     }
 }
