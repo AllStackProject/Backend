b4b530935878d4f7f2944ed7f3de40f3fd6e3bef
hyunn522 <tjgus52213@gmail.com>
2025-11-10 21:48:07 +0900

    fix: @Transactional 어노테이션 추가ㅋ

--- src/main/java/app/allstackproject/privideo/service/CommentService.java
+++ src/main/java/app/allstackproject/privideo/service/CommentService.java
@@ -20,15 +20,18 @@ import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
+@Transactional
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final VideoRepository videoRepository;
     private final MemberRepository memberRepository;
 
+    @Transactional(readOnly = true)
     public CommentResponse getUserComments(Long memberId, Long orgId) {
         List<Comment> commentList = commentRepository.findByMemberIdAndVideoOrganizationId(memberId, orgId);
         return CommentResponse.of(commentList);
@@ -47,6 +50,7 @@ public class CommentService {
         return true;
     }
 
+    @Transactional(readOnly = true)
     public CommentsResult readVideoComments(Long memberId, Long orgId, Long videoId) {
         if (!videoRepository.isValidMemberAndOrgAndVideo(memberId, orgId, videoId)) {
             throw new ApiException(INVALID_COMMENT_REQUEST);
--- src/main/java/app/allstackproject/privideo/service/video/HistoryService.java
+++ src/main/java/app/allstackproject/privideo/service/video/HistoryService.java
@@ -7,13 +7,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class HistoryService {
 
     private final HistoryRepository historyRepository;
 
+    @Transactional(readOnly = true)
     public HistoryResponse getUserVideos(Long memberId, Long orgId) {
         List<History> histories = historyRepository.findByMemberIdAndVideoOrganizationId(memberId, orgId);
         return HistoryResponse.of(histories);
