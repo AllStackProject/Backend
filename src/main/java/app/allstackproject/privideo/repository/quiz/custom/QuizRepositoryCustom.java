6f73bd39dbf79c45aedfd60436167bb002f457c8
hyunn522 <tjgus52213@gmail.com>
2025-11-13 10:51:34 +0900

    feat: 퀴즈 결과 저장 관련 로직 삭제

--- src/main/java/app/allstackproject/privideo/repository/quiz/custom/QuizRepositoryCustom.java
+++ src/main/java/app/allstackproject/privideo/repository/quiz/custom/QuizRepositoryCustom.java
@@ -1,11 +1,8 @@
 package app.allstackproject.privideo.repository.quiz.custom;
 
-import app.allstackproject.privideo.dto.quiz.MemberQuizDto;
 import app.allstackproject.privideo.dto.video.QuizInfo;
 import java.util.List;
 
 public interface QuizRepositoryCustom {
-    List<MemberQuizDto> findByMemberIdAndOrganizationId(Long memberId, Long orgId);
-
     List<QuizInfo> findByVideoId(Long videoId);
 }
