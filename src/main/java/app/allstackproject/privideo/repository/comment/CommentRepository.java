135d29a22be08c1b93da8bebedfaef77a46d5c15
hyunn522 <tjgus52213@gmail.com>
2025-11-20 14:19:52 +0900

    fix: 댓글 조회 시 N+1 문제 해결

--- src/main/java/app/allstackproject/privideo/repository/comment/CommentRepository.java
+++ src/main/java/app/allstackproject/privideo/repository/comment/CommentRepository.java
@@ -2,6 +2,7 @@ package app.allstackproject.privideo.repository.comment;
 
 import app.allstackproject.privideo.entity.Comment;
 import app.allstackproject.privideo.repository.comment.custom.CommentRepositoryCustom;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,6 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
+    @EntityGraph(attributePaths = {"video"})
     List<Comment> findByMemberIdAndVideoOrganizationId(Long memberId, Long orgId);
 }
